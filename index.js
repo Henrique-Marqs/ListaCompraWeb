067ab27955231dd2df986fe462ca511a02e7b464
Henrique <uchihcl@gmail.com>
2025-07-25 19:50:32 -0300

    first-commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const inputItem = document.getElementById("input-item")
 
-console.log(inputItem.value);
+
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+import verificarListaVazia from "./scripts/verificarListaVazia.js";
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const itemDaLista = criarItemDaLista();
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
+
+})
+
+
+verificarListaVazia(listaDeCompras);
